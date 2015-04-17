699d0a4f6843a79ce1268e08a2dc1f8c12dd47d1
Jack <jack.sy@live.com>
2015-04-17 11:33:52 -0400

    flash notice

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
     if @user.persisted?
-      flash[:notice] = "Welcome, #{@user.username}"
+      flash[:notice] = "Hi, #{@user.username}. Check out the new events."
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.twitter_uid"] = request.ENV["omniauth.auth"]
