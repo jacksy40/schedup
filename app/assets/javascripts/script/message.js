3a8ff36e64b356f0ab8c7a2000d3e660eda2ec29
Jack <jack.sy@live.com>
2015-04-24 20:01:20 -0400

    topbar update

--- app/assets/javascripts/script/message.js
+++ app/assets/javascripts/script/message.js
@@ -2,26 +2,24 @@
     $('#new_message').on('ajax:complete', function(e, data, status, xhr){
       $('#message_message').val('');
         var mObject = JSON.parse(data.responseText);
-        var event_id = parseFloat(mObject.event_id);
-          $(function messageCall( ) {
-            var messageRequest = $.get( '/events/' + event_id + '/messages', function(e, data, status, xhr ) {
-            var messages = JSON.parse(data);
-            debugger;
-            messages.forEach(function(){});
-            for (i = 0; i < messages.length; i++) {
-            $( ".chatter_box" ).append('<li>' + data[i] + '<li>');
-            }
+        var eventId = parseInt(mObject.event_id);
 
-            debugger;
-      messageRequest.done(function(messages) {
-      var data = JSON.parse(messages);
-      for (i = 0; i < data.length; i++) {
-        $('#titles-list').append('<li>' + data[i] + '<li>');
-      }
-        });
-      });
+      $(function messageCall() {
+      var messageRequest = $.get( '/events/20/messages', function(event, data, status, xhr) {
+      debugger;
+      var messages = JSON.parse(data);
     });
   });
 });
+
+//       messageRequest.done(function(messages) {
+//       messages.forEach(function(){
+//       $( '.chatter_box' ).append('<li>' + data[i] + '<li>');
+//       });
+//     });
+//   });
+// });
+
+
 
 
--- app/models/selectdate.rb
+++
@@ -1,8 +0,0 @@
-class Selectdate < ActiveRecord::Base
-  belongs_to :user
-  has_many :tasks
-
-  validates :date, presence: true
-  validates :user_id, presence: true
-end
-
